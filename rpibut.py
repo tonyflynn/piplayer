78af93daf1dc4eb223c23ab92211377166ba1a6a
tonyflynn <tonyflynn@gmail.com>
2013-01-17 20:39:39 +1100

    Finalise test button code

--- rpibut.py
+++ rpibut.py
@@ -4,9 +4,9 @@ import os
 import RPi.GPIO as GPIO 
 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(23, GPIO.IN)
+GPIO.setup(15, GPIO.IN)
 
 while True:
-	if ( GPIO.input(23) != False ):
+	if ( GPIO.input(15) != True ):
 		print("Button press...")
-	sleep(0.3);
+	sleep(0.2);
