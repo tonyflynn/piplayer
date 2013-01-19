aee51b4204c91a2b0a7c6b4f928a6f873417bd6f
tonyflynn <tonyflynn@gmail.com>
2013-01-19 18:51:08 +1100

    Added LCD control for Pandora

--- lcdctrl.py
+++ lcdctrl.py
@@ -2,8 +2,9 @@
 
 from lcdlib import *
 from time import sleep
+import os
 import sys
-from subprocess import *
+#from subprocess import *
 
 # Add the SIGINT handler
 signal.signal(signal.SIGINT, signal_handler)
@@ -19,22 +20,35 @@ station = ""
 artist = ""
 song = ""
 
+player = "pandora"
+#player = "mpc"
 while True:
-  mpcinfo = run_cmd("mpc current")
-  mpcinfo = mpcinfo.strip()
-  # check that the data is valid ie. contains a colon
-  if (mpcinfo.count(": ") != 0):
-    channeldata = mpcinfo.split(": ")
-    station = channeldata[0].strip()
-    if len(channeldata) > 1:
-      songdata = channeldata[1].split(" - ")
-      artist = songdata[0].strip()
-      song = songdata[1].strip()
+  if player == "mpc":
+    #print os.environ.get('PIPLAYER')
+    mpcinfo = run_cmd("mpc current")
+    mpcinfo = mpcinfo.strip()
+    # check that the data is valid ie. contains a colon
+    if (mpcinfo.count(": ") != 0):
+      channeldata = mpcinfo.split(": ")
+      station = channeldata[0].strip()
+      if len(channeldata) > 1:
+        songdata = channeldata[1].split(" - ")
+        artist = songdata[0].strip()
+        song = songdata[1].strip()
+      else:
+        artist = ""
+        song = ""
     else:
-      artist = ""
-      song = ""
-  else:
-    station = ""
+      station = ""
+  elif player == "pandora":
+    # Get song info from Pandora out file: pandoraout
+    f = open('pandoraout', 'r')
+    pandorainfo = f.read()
+    f.close
+    pandorainfo =  pandorainfo.split("|")
+    station = "Pandora: " + pandorainfo[0].strip()
+    song = pandorainfo[1].strip()
+    artist = pandorainfo[2].strip()
 
   # we have the results, now scroll them on the LCD
   lcd_scroll(station, "", song, artist)
