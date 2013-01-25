27df2efef0c5b1ea6f64577f2a015534b4dfe4fe
tonyflynn <tonyflynn@gmail.com>
2013-01-25 20:47:54 +1100

    Split player functions to seperate script

---
+++ play_pandora.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+### Control prog for Pandora ###
+
+import subprocess
+import os
+import sys
+import signal
+from time import sleep
+import RPi.GPIO as GPIO
+
+# set up GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15, GPIO.IN) # test button
+
+def main():
+  # Add the SIGINT handler
+  signal.signal(signal.SIGINT, signal_handler_main)
+
+  print "Starting Pandora..."
+  pp = subprocess.Popen(["sudo", "-u", "pi", "pianobar"])
+  #os.system("pianobar")
+  # Start the LCD script
+  lp = subprocess.Popen(["/home/pi/piplayer/lcdctrl.py", "pandora"])
+
+  exitPlayer = False
+  ### BEGIN THE LOOP ###
+  while exitPlayer == False:
+    if (GPIO.input(15) == False): # Button 1 pressed
+      # Stop Pandora
+      os.system("echo 'q' >> /home/pi/.config/pianobar/ctl")
+      exitPlayer = True
+  ### END OF LOOP ###
+#  sleep(10)
+  kill_procs()
+
+def signal_handler_main(signal, frame):
+  # handle interrupt
+  print("Closing other processes...")
+  # Kill subprocesses
+  kill_procs()
+  #lp.send_signal(signal.SIGINT)
+  # Exit
+  sys.exit(0)
+
+def kill_procs():
+  # kill any sub processes we've started in case we have any rogues
+  # send SIGINT so they close happily
+  # have signal handlers on anything you kill here...
+  os.system("sudo pkill -2 lcdctrl.py")
+  #lp.send_signal(signal.SIGINT)
+
+if __name__ == "__main__":
+    main()
+
